1b789b100c0f42e5a91946675a9a841708a342fa
이정인 <vbnmj123@naver.com>
2026-01-29 16:13:26 +0900

    Level 3. 세로 스택 뷰 생성

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     // Level 1. UILabel 사용.
     let label = UILabel()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -41,26 +41,38 @@ class ViewController: UIViewController {
         }
         
         
-        // Level 2. UIStackView을 사용해서 4개의 버튼을 모아 가로 스택 뷰 생성.
-        let buttonTitles = ["7", "8", "9", "+"] // titles 상수 안에 배열 형식으로 숫자 7, 8, 9, +를 넣어줌.
+        // Level 3. UIStackView을 사용해서 세로 스택 뷰 생성.
+        let buttonTitles = [ // buttonTitles 상수 안에 배열 생성.
+            ["7", "8", "9", "+"],
+            ["4", "5", "6", "-"],
+            ["1", "2", "3", "*"],
+            ["AC", "0", "=", "/"]
+        ]
         
-        let buttons = buttonTitles.map { titles in // buttonTitles 배열의 각 문자열("7", "8", "9", "+")을 하나씩 꺼내서 buttons에 UIButton 형태로 배열이 저장됨.
+        // horziontalStackView 배열.
+        let horziontalStackView = buttonTitles.map { titles in // 고차함수 중에 map을 사용하여 배열의 각 요소를 변환하여 새로운 배열을 생성.
             
-            makeButton(title: titles)
+            makeHorizontalStackView(titles.map { makeButton(title: $0) })
         }
         
-        let horizontalStackView = makeHorizontalStackView(buttons) // 방금 만든 버튼 배열을 전달해서 버튼 4개가 가로로 정렬된 UIStackView를 생성.
-        view.addSubview(horizontalStackView)
         
-        // horizontalStackView의 제약 조건
-        horizontalStackView.snp.makeConstraints {
-            $0.height.equalTo(80) // StackView의 높이를 80만큼 설정.
-            $0.leading.trailing.equalToSuperview().inset(36) // View로 부터 왼쪽과 오른쪽으로 부터 36만큼 떨어지게 설정.
-            $0.top.equalTo(label.snp.bottom).offset(50) // Label의 bottom과 StackView의 top의 거리를 50만큼 떨어지게 설정.
+        // verticalStackView의 속성.
+        let verticalStackView = UIStackView(arrangedSubviews: horziontalStackView)
+        verticalStackView.axis = .vertical // 수직(세로) 축.
+        verticalStackView.backgroundColor = .black // verticalStackViewd의 배경색은 검은색.
+        verticalStackView.spacing = 10 // 스택 뷰 안에 들어있는 뷰들의 거리 10 설정.
+        verticalStackView.distribution = .fillEqually // 분배, 스택 뷰 내부의 사이즈 분배에 관한 설정. fillEqually를 선택하면 뷰들의 사이즈가 동일하게 맞춰진다.
+        
+        view.addSubview(verticalStackView)
+        
+        // verticalStackView의 제약 조건
+        verticalStackView.snp.makeConstraints {
+            $0.width.height.equalTo(350) // 가로와 세로의 길이를 350으로 설정.
+            $0.top.equalTo(label.snp.bottom).offset(60) // top = label 의 bottom 으로 부터 60 떨어지도록.
+            $0.centerX.equalToSuperview() //
         }
-        
+ 
     }
-    
     
     
     // makeButton 메서드를 생성하여 button의 속성.
@@ -91,4 +103,5 @@ class ViewController: UIViewController {
     }
     
 }
+
 
